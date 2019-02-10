9e2995ec41b61b35844ef663944d6fd0b92ef854
vanik.karslian <vanik.karslian@factor-a.de>
2019-02-10 22:22:32 +0100

    ad1 added changes in server

--- arduino1/actuatorServer/actuatorServer.ino
+++ arduino1/actuatorServer/actuatorServer.ino
@@ -23,16 +23,15 @@ char buf_content[BUF_LEN];
 int counter = 0;
 
 //const char token1[] PROGMEM = "GET /off ";
-char token1[] = "GET /off ";
-char token2[] = "PUT /off";
-char token3[] = "PUT /on";
-char token4[] = "GET /on";
-char token5[] = "GET / ";
+
+char token2[] = "PUT /uni-bonn/raum1047/led";
+
+
 
 void setup() {
   // setup code to run once:
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(9060);
   while (!Serial) {
     ; // wait for serial port to connect
   }
@@ -40,19 +39,20 @@ void setup() {
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("starting... ");
-  Serial.println(Ethernet.localIP());
   //starting LED to show that the microcontroller is working
   pinMode(LED_BUILTIN, OUTPUT);
 
-//  Serial.println(Ethernet.localIP());
+  Serial.println(Ethernet.localIP());
 
   //reserve full buffer with EOS (end of string)
   memset(buf_content, 0, BUF_LEN);
-
   pinMode(fanPin, OUTPUT);
+  
 }
 
 void loop() {
+
+
   // main code to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 
@@ -62,12 +62,13 @@ void loop() {
     Serial.println("new client");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
-
+    char buffer[746];
+    int index = 0;
     //Response of LDFU is processed
     while (client.connected()) {
       if (client.available()) {
+       
         char c = client.read();
-        
         //saves the first line of header to assign appropriate response later
         if (req_index < (REQ_LEN - 1)) { 
           buf_req[req_index] = c;
@@ -77,57 +78,61 @@ void loop() {
         
         //if end of line is reached and currentLineIsBlank is true then following
         if (c == '\n' && currentLineIsBlank) { 
-          //saves content in field
-          Serial.println("Start to read the request body");
-          for (int i = 0; i < BUF_LEN; i++) { 
-            buf_content[i] = client.read();;
-            Serial.println(buf_content[i]);
+          
+          for (int i = 0; i < BUF_LEN; i++) 
+          { 
+            buf_content[i] = client.read();
+            if(index < 745)
+            {
+              buffer[index] = buf_content[i];
+              index++;
+              buffer[index] = '\0';
+            }
+            
             // if client is not available and therefore full content is saved, break
             if (client.available() == 0) { 
               break;
-            }            
+            }
             counter++; // counter indicates length of the field (900 characters are reserved)
+              
           }
-          Serial.println("Finish to read the request body");
-          Serial.println("counter: " );
-          Serial.println("+ counter");
+            Serial.print(buffer);
           // if loops look for appropriate response
-          // OFF response
-          if ((strstr(buf_req, token1) != 0) || (strstr(buf_req, token2) != 0)) {
+          // ON response
+          if ((strstr(buf_req, token2) != 0   && strstr(buffer, "TurnOn") != NULL)) 
+          {
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: application/rdf+xml");
+            client.println("Content-Type: RDF/XML");
             client.print("Content-Length: ");
             client.println(counter);
             client.println("Connection: close");
             client.println();
             //sends received content of the request back
-            for (int i = 0; i < counter; i++) {
-              client.print(buf_content[i]);
-            }
-            //turning fan off
+            //for (int i = 0; i < counter; i++) {
+              //client.print(buf_content[i]);
+            //}
             digitalWrite(fanPin, HIGH);
+             
+                      
           }
-          // ON response
-          else if ((strstr(buf_req, token3) != 0) || (strstr(buf_req, token4) != 0)) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: application/rdf+xml");
+            // off response
+          else if((strstr(buf_req, token2) != 0  && strstr(buffer, "TurnOff") != NULL)){
+             client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: RDF/XML");
             client.print("Content-Length: ");
             client.println(counter);
+            Serial.print("sending response...");
             client.println("Connection: close");
             client.println();
-            for (int i = 0; i < counter; i++) {
-              client.print(buf_content[i]);
-            }
-            //turning fan on
-            digitalWrite(fanPin, LOW);
+            //sends received content of the request back
+            //for (int i = 0; i < counter; i++) {
+              //client.print(buf_content[i]);
+            //}
+            //turning fan off
+           digitalWrite(fanPin, LOW);
+           
+            
           }
-          // GET on ip adresse
-          else if (strstr(buf_req, token5) != 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Connection: close");
-            client.println();
-          }
-          // wrong request
           else {
             client.println("HTTP/1.1 404 Not Found"); //204 No Content
             client.println("Connection: Keep Alive");
